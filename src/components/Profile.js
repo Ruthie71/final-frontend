79077bb2b11523b4e1661522a86c3859e893be38
Ruthie <ruthmckinney1971@gmail.com>
2021-08-26 16:27:11 +0200

    styling added to Profile nav

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,21 +5,36 @@ import ContactInfo from "./ContactInfo";
 import Education from "./Education";
 import WorkExperience from "./WorkExperience";
 import ProfileInfo from "./ProfileInfo";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 // import NotFound from "./NotFound";
 
 const Profile = ({ match }) => {
     return (
         <Fragment>
             <TabSystem />
-            <Switch>
-                <Route path={"/profile/contactInfo"} component={ContactInfo} />
-                <Route path={"/profile/profileInfo"} component={ProfileInfo} />
-                <Route path={"/profile/education"} component={Education} />
-                <Route
-                    path={"/profile/workexperience"}
-                    component={WorkExperience}
-                />
-            </Switch>
+            <Container>
+                <Row className="mt-5 justify-content-center">
+                    <Switch>
+                        <Route
+                            path={"/profile/contactInfo"}
+                            component={ContactInfo}
+                        />
+                        <Route
+                            path={"/profile/profileInfo"}
+                            component={ProfileInfo}
+                        />
+                        <Route
+                            path={"/profile/education"}
+                            component={Education}
+                        />
+                        <Route
+                            path={"/profile/workexperience"}
+                            component={WorkExperience}
+                        />
+                    </Switch>
+                </Row>
+            </Container>
         </Fragment>
     );
 };
