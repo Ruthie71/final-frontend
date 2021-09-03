dbcdaab6fa62425e5166e2c24a1781c05097cd97
AlexandruCiripoi <alexandru.ciripoi@gmail.com>
2021-09-03 18:49:36 +0200

    tags sysyem

--- src/components/KeyWords.js
+++ src/components/KeyWords.js
@@ -1,5 +1,40 @@
-const KeyWords = () => {
-    return <div>Key words</div>;
+import { useRef } from 'react';
+import ReactTags from 'react-tag-autocomplete';
+import "./KeyWords.css"
+
+
+const KeyWords = ({ tags, setTags, suggestions, noSuggestionsText }) => {
+  const reactTags = useRef();
+  const onDelete = i => setTags(prev => prev.filter(t => t._id !== tags[i]._id));
+
+  const onAddition = tag => setTags(prev => [...prev, tag]);
+
+  return (
+    <div style={{ margin: 10 }}>
+      <ReactTags
+        ref={reactTags}
+        tags={tags}
+        suggestions={suggestions}
+        noSuggestionsText={noSuggestionsText}
+        onDelete={onDelete}
+        onAddition={onAddition}
+        classNames={{
+          root: 'form-control',
+          rootFocused: 'form-control:focus',
+          selected: 'react-tags__selected',
+          selectedTag: 'react-tags__selected-tag',
+          selectedTagName: 'react-tags__selected-tag-name',
+          search: 'react-tags__search',
+          searchWrapper: 'react-tags__search-wrapper',
+          searchInput: 'react-tags__search-input',
+          suggestions: 'react-tags__suggestions',
+          suggestionActive: 'is-active',
+          suggestionDisabled: 'is-disabled',
+          suggestionPrefix: 'react-tags__suggestion-prefix'
+        }}
+      />
+    </div>
+  );
 };
 
 export default KeyWords;
